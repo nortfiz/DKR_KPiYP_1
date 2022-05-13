6a886909a7d843d3a2dfe5f20362a53bd74e4040
nortfiz <chantr_2@icloud.com>
2022-05-13 10:55:39 +0300

    Добавьте файлы проекта.

---
+++ DKR_KPiYP_1/Zadanie_1.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите а");
+int a = int.Parse(s: Console.ReadLine());
+
+Console.WriteLine("Введите b");
+int b = int.Parse(s: Console.ReadLine());
+
+Console.WriteLine("Введите h");
+int h = int.Parse(s: Console.ReadLine());
+
+int k = 1;
+Console.WriteLine("Введите N");
+int n = int.Parse(Console.ReadLine());
+
+double q = 0;
+
+for (int x = a + h; x <= b; x += h)
+{
+    Console.WriteLine("Х равно:" + x);
+    double S = x;
+    for (int i = 0; i < n; i++)
+    {
+        double r = (Math.Pow(-1, k + 1)) * (Math.Pow(S, 2 * k)) / (2 * k * (2 * k - 1));
+        k++;
+        q = q + r;
+       
+    }
+     Console.WriteLine("S(x)=  "+q);
+    
+        double r_2 = S * Math.Atan(S) - Math.Log(Math.Sqrt(1 + Math.Pow(S, 2)), 10);
+        Console.WriteLine("Y(x) = " + r_2);
+
+        double r_3 = Math.Abs(r_2 - q);
+        Console.WriteLine("Y(x)-S(x) = " + r_3);
+ }
