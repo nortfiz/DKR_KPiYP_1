903c194b57b4a0883f79c1dd430b79cb875d22e1
nortfiz <chantr_2@icloud.com>
2022-05-13 14:35:03 +0300

    Egor

---
+++ Zadanie_2/Zadanie_2.cs
@@ -0,0 +1,24 @@
+﻿int n = 8;
+int m = 8;
+int[,] mat;
+mat = new int[n, m];
+Random random = new Random();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        mat[i, j] = random.Next(100);
+        Console.Write("{0,4}", mat[i, j]);
+    }
+    Console.WriteLine();
+
+}
+for(int j = 0; j < n; j++)
+            {
+    int count = 0;
+    for (int i = 0; i < m; i++)
+        if (mat[i, j] == 1)
+            count++;
+    if (count == m)
+        Console.WriteLine("column({0})", j + 1);
+}
